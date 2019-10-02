74b384054e11c8b47fe21b1c45a09cbf9f5e743e
Mélanie Liraud-Coël <melanie.liraud@gmail.com>
2019-10-02 11:21:13 +0200

    Ajout des noms en couleur

---
+++ wilders.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+
+console.log(chalk.red('Violette Cruau'));
+console.log(chalk.yellow('Alexis Barbier'));
+console.log(chalk.green('Maxence Vanmarcke'));
+console.log(chalk.blue('Baptiste Nicolas'));
